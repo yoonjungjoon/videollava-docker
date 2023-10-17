d95791b834e6c760912f289da5358357cda9d362
Ashley Kleynhans <ashley.kleynhans@gmail.com>
2023-10-17 13:50:13 +0200

    Use the correct model

--- scripts/start_model_worker.sh
+++ scripts/start_model_worker.sh
@@ -6,5 +6,5 @@ nohup python3 -m llava.serve.model_worker \
   --controller http://localhost:${LLAVA_CONTROLLER_PORT} \
   --port ${LLAVA_MODEL_WORKER_PORT} \
   --worker http://localhost:${LLAVA_MODEL_WORKER_PORT} \
-  --model-path liuhaotian/llava-v1.5-13b > /workspace/logs/model-worker.log 2>&1 &
+  --model-path ${LLAVA_MODEL} > /workspace/logs/model-worker.log 2>&1 &
 deactivate
